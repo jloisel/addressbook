2724952182dd886b0ca7a7139d36f44b7332bf62
Jerome <loisel.jerome@gmail.com>
2013-09-20 17:15:07 +0200

    coding in mvp style

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -1,25 +1,14 @@
 package com.vaadin.tutorial.addressbook;
 
 import com.vaadin.annotations.Title;
-import com.vaadin.data.Property;
-import com.vaadin.data.Property.ValueChangeEvent;
-import com.vaadin.data.fieldgroup.BeanFieldGroup;
-import com.vaadin.data.fieldgroup.FieldGroup;
-import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.event.FieldEvents.TextChangeEvent;
-import com.vaadin.event.FieldEvents.TextChangeListener;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.AbstractTextField.TextChangeEventMode;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.HorizontalSplitPanel;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.TextField;
+import com.vaadin.tutorial.addressbook.model.ContactModel;
+import com.vaadin.tutorial.addressbook.model.Models;
+import com.vaadin.tutorial.addressbook.presenter.Presenter;
+import com.vaadin.tutorial.addressbook.presenter.Presenters;
+import com.vaadin.tutorial.addressbook.view.ContactView;
+import com.vaadin.tutorial.addressbook.view.Views;
 import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
 /* 
  * UI class is the starting point for your app. You may deploy it with VaadinServlet
@@ -28,178 +17,19 @@ import com.vaadin.ui.VerticalLayout;
  * embed your UI to an existing web page. 
  */
 @Title("Addressbook")
-public class AddressbookUI extends UI {
-
-	/* User interface components are stored in session. */
-	private Table contactList = new Table();
-	private TextField searchField = new TextField();
-	private Button addNewContactButton = new Button("New");
-	private Button removeContactButton = new Button("Remove this contact");
-	private FormLayout editorLayout = new FormLayout();
-	private FieldGroup editorFields = new BeanFieldGroup<Contact>(Contact.class);
-
-	/*
-	 * Any component can be bound to an external data source. This example uses
-	 * just a dummy in-memory list, but there are many more practical
-	 * implementations.
-	 */
-	private final BeanItemContainer<Contact> contactContainer = Fixtures.createRandomContacts();
+public final class AddressbookUI extends UI {
 
 	/*
 	 * After UI class is created, init() is executed. You should build and wire
 	 * up your user interface here.
 	 */
 	@Override
-	protected void init(VaadinRequest request) {
-		initLayout();
-		initContactList();
-		initEditor();
-		initSearch();
-		initAddRemoveButtons();
+	protected void init(final VaadinRequest request) {
+		final ContactView view = Views.newView();
+		final ContactModel model = Models.newModel();
+		final Presenter presenter = Presenters.newPresenter(view, model);
+		setContent(view);
+		
+		presenter.onViewOpen();
 	}
-
-	/*
-	 * In this example layouts are programmed in Java. You may choose use a
-	 * visual editor, CSS or HTML templates for layout instead.
-	 */
-	private void initLayout() {
-
-		/* Root of the user interface component tree is set */
-		HorizontalSplitPanel splitPanel = new HorizontalSplitPanel();
-		setContent(splitPanel);
-
-		/* Build the component tree */
-		VerticalLayout leftLayout = new VerticalLayout();
-		splitPanel.addComponent(leftLayout);
-		splitPanel.addComponent(editorLayout);
-		leftLayout.addComponent(contactList);
-		HorizontalLayout bottomLeftLayout = new HorizontalLayout();
-		leftLayout.addComponent(bottomLeftLayout);
-		bottomLeftLayout.addComponent(searchField);
-		bottomLeftLayout.addComponent(addNewContactButton);
-
-		/* Set the contents in the left of the split panel to use all the space */
-		leftLayout.setSizeFull();
-
-		/*
-		 * On the left side, expand the size of the contactList so that it uses
-		 * all the space left after from bottomLeftLayout
-		 */
-		leftLayout.setExpandRatio(contactList, 1);
-		contactList.setSizeFull();
-
-		/*
-		 * In the bottomLeftLayout, searchField takes all the width there is
-		 * after adding addNewContactButton. The height of the layout is defined
-		 * by the tallest component.
-		 */
-		bottomLeftLayout.setWidth("100%");
-		searchField.setWidth("100%");
-		bottomLeftLayout.setExpandRatio(searchField, 1);
-
-		/* Put a little margin around the fields in the right side editor */
-		editorLayout.setMargin(true);
-		editorLayout.setVisible(false);
-	}
-
-	private void initEditor() {
-
-		editorLayout.addComponent(removeContactButton);
-
-		/* User interface can be created dynamically to reflect underlying data. */
-		for (final String fieldName : contactContainer.getContainerPropertyIds()) {
-			final TextField field = new TextField(fieldName);
-			field.setValidationVisible(true);
-			editorLayout.addComponent(field);
-			field.setWidth("100%");
-
-			/*
-			 * We use a FieldGroup to connect multiple components to a data
-			 * source at once.
-			 */
-			editorFields.bind(field, fieldName);
-		}
-
-		/*
-		 * Data can be buffered in the user interface. When doing so, commit()
-		 * writes the changes to the data source. Here we choose to write the
-		 * changes automatically without calling commit().
-		 */
-		editorFields.setBuffered(false);
-	}
-
-	private void initSearch() {
-
-		/*
-		 * We want to show a subtle prompt in the search field. We could also
-		 * set a caption that would be shown above the field or description to
-		 * be shown in a tooltip.
-		 */
-		searchField.setInputPrompt("Search contacts");
-
-		/*
-		 * Granularity for sending events over the wire can be controlled. By
-		 * default simple changes like writing a text in TextField are sent to
-		 * server with the next Ajax call. You can set your component to be
-		 * immediate to send the changes to server immediately after focus
-		 * leaves the field. Here we choose to send the text over the wire as
-		 * soon as user stops writing for a moment.
-		 */
-		searchField.setTextChangeEventMode(TextChangeEventMode.LAZY);
-
-		/*
-		 * When the event happens, we handle it in the anonymous inner class.
-		 * You may choose to use separate controllers (in MVC) or presenters (in
-		 * MVP) instead. In the end, the preferred application architecture is
-		 * up to you.
-		 */
-		searchField.addTextChangeListener(new TextChangeListener() {
-			@Override
-			public void textChange(final TextChangeEvent event) {
-
-				/* Reset the filter for the contactContainer. */
-				contactContainer.removeAllContainerFilters();
-				contactContainer.addContainerFilter(new ContactFilter(event
-						.getText()));
-			}
-		});
-	}
-
-	private void initAddRemoveButtons() {
-		addNewContactButton.addClickListener(new NewContactClick(contactContainer, contactList));
-
-		removeContactButton.addClickListener(new ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				Object contactId = contactList.getValue();
-				contactList.removeItem(contactId);
-			}
-		});
-	}
-
-	private void initContactList() {
-		contactList.setContainerDataSource(contactContainer);
-		contactList.setSelectable(true);
-		contactList.setImmediate(true);
-
-		contactList.addValueChangeListener(new Property.ValueChangeListener() {
-			@Override
-			public void valueChange(ValueChangeEvent event) {
-				Object contactId = contactList.getValue();
-
-				/*
-				 * When a contact is selected from the list, we want to show
-				 * that in our editor on the right. This is nicely done by the
-				 * FieldGroup that binds all the fields to the corresponding
-				 * Properties in our contact at once.
-				 */
-				if (contactId != null)
-					editorFields.setItemDataSource(contactList
-							.getItem(contactId));
-				
-				editorLayout.setVisible(contactId != null);
-			}
-		});
-	}
-
 }
